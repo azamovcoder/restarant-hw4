e7b31454fd29bd5fc361e50c4da57219cb213780
azamovcoder <azamovcoder@gmail.com>
2024-03-06 21:34:36 +0300

    all

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector(".header__hamburger");
-let navBar =document.querySelector(".header__list");
-
-
-menu.addEventListener("click", function ()
-{
-    navBar.classList.toggle("header__show-list");
-});
